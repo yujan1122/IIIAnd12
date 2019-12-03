105c645770b91412a50c718e7fb25d4322657a4a
Annie <https://github.com/yujan1122/practice1.git>
2019-12-03 09:24:54 +0800

    3 new fragment

---
+++ app/src/main/java/tw/org/iii/iiiand12/P1.java
@@ -0,0 +1,26 @@
+package tw.org.iii.iiiand12;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class P1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_p1, container, false);
+    }
+
+}
